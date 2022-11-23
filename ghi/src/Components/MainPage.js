cdd9d9141262d2259192747e8d01435d36d776aa
Mary McKinnon <your@email.heremaryamckinnon@gmail.com>
2022-11-22 21:03:10 -0800

    Making styling changes to main page along with adjusting animations

--- ghi/src/Components/MainPage.js
+++ ghi/src/Components/MainPage.js
@@ -7,6 +7,9 @@ import HeroBanner from './HeroBanner';
 import Modal from 'react-bootstrap/Modal';
 import AOS from 'aos'
 import 'aos/dist/aos.css'
+import activities from '../images/activities.png';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function MainPage() {
 
@@ -14,53 +17,58 @@ function MainPage() {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const handleScroll = event => {
-    console.log('scrollTop: ', event.currentTarget.scrollTop);
-    console.log('offsetHeight: ', event.currentTarget.offsetHeight);
-  };
-
   return (
     <>
     <HeroBanner />
-    
       <Container>
-          <h1
-            className="citysearch-header"
-            data-aos="fade-up"
-            style={{ fontSize: "1.6rem", textAlign:'center', marginTop:'10%' }}
-          >
-            <span className="font-link2">
-              <div>
-                Know what you want to do <br />
+        <Row>
+          <Col>
+            <Container data-aos="fade-right">
+              <h1
+                className="citysearch-header"
+                
+                style={{ fontSize: "1.6rem", textAlign:'center', marginTop:'45%' }}
+              >
+                <span className="font-link2">
+                  <div>
+                    Know what you want to do <br />
+                  </div>
+                  <div>but not sure where to do it?</div>
+                </span>
+              </h1>
+              <div className="arrow-search" style={{marginTop:'5%'}}>
+                <Button
+                    variant='dark'
+                    className='cat-search'
+                    onClick={handleShow}
+                  >
+                    Search
+                </Button>
+                <div className='cat-search-modal'>
+                  <Modal 
+                    show={show} 
+                    onHide={handleClose} 
+                    dialogClassName='modal-md'
+                    style={{textAlign:'center', display:'block'}}
+                  >
+                    <Modal.Header closeButton>Search an activity in a group of cities
+                    </Modal.Header>
+                    <Modal.Body style={{maxWidth:470}}>
+                      <CategorySearch />
+                    </Modal.Body>
+                    <Modal.Footer>
+                    </Modal.Footer> 
+                  </Modal>
+                </div>
               </div>
-              <div>but not sure where to do it?</div>
-            </span>
-          </h1>
-          <div className="arrow-search">
-            <Button
-                variant='dark'
-                className='cat-search'
-                onClick={handleShow}
-              >
-                Click Here!
-            </Button>
-            <div className='cat-search-modal'>
-              <Modal 
-                show={show} 
-                onHide={handleClose} 
-                dialogClassName='modal-md'
-                style={{textAlign:'center', display:'block'}}
-              >
-                <Modal.Header closeButton>Search an activity in a group of cities
-                </Modal.Header>
-                <Modal.Body style={{maxWidth:470}}>
-                  <CategorySearch />
-                </Modal.Body>
-                <Modal.Footer>
-                </Modal.Footer> 
-              </Modal>
-            </div>
-          </div>
+            </Container>
+        </Col>
+        <Col>
+          <Container style={{marginTop:'25%', marginLeft:'10%'}} data-aos="fade-left">
+            <img src={activities} height={350}></img>
+          </Container>
+        </Col>
+        </Row>
       </Container>
       <Container>
         <SuggestionList />
